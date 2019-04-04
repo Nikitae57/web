78bbf9094522d8862ae9a34d829c1f0ae4a82f14
Nikita Evdokimov <nikita-e57@yandex.ru>
2019-04-04 11:13:09 +0300

    Added lab 3 and 4

---
+++ lab4/about.php
@@ -0,0 +1,18 @@
+ <!DOCTYPE html>
+ <html lang="ru">
+ 
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Мини-блог</title>
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+ </head>
+
+ <body>
+ 	<?php 
+ 		include_once "header.php";
+ 		include_once "navigation.php";
+ 		include_once "contents/content_about.php";
+ 		include_once "footer.php";
+ 	?>
+ </body>
+ </html>
